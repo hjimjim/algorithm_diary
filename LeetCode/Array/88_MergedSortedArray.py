c5278a28c0c6b35fa8869708c44e3bd691849c9c
Jimin Heo <jimjim.heo@gmail.com>
2023-06-14 15:46:44 +0900

    Top Interview 150 (Array/ Easy/ Jun 14, 2023)

--- LeetCode/Array/88_MergedSortedArray.py
+++ LeetCode/Array/88_MergedSortedArray.py
@@ -1,11 +1,13 @@
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
+        Top Interview 150 (Array/ Easy)
+        Date : Jun 14, 2023
+        
+        Runtime : 50ms, faster than 63.54%
         very simple approach 
         1. merge two lists
         2. sort it
-
-        Runtime : 50ms, faster than 63.54%
         """
         nums1[m:] = nums2
         nums1.sort()
