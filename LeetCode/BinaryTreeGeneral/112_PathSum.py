ca64c62978ed05d9860a4a12a5a22fb07eae38b0
Jimin Heo <jimjim.heo@gmail.com>
2023-07-07 23:27:45 +0900

    Top Interview 150 (BinaryTree | Easy | Jul 7, 2023) - good solution

--- LeetCode/BinaryTreeGeneral/112_PathSum.py
+++ LeetCode/BinaryTreeGeneral/112_PathSum.py
@@ -11,25 +11,37 @@ class Solution:
         112. Path Sum
         link : https://leetcode.com/problems/path-sum/
         Date : July 7, 2023
-        
+
         Runtime : 55 ms, faster than 78.55%
     """
     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
-        if not root: 
-            return False
-        result = []
         def sum(head, res):
+            if not head: return False
+            res += head.val
             if not head.left and not head.right:
-                res += head.val
-                result.append(res)
+                return targetSum == res
             
-            if not head.left and head.right:
-                sum(head.right, res + head.val)
-            elif not head.right and head.left:
-                sum(head.left, res + head.val)
-            elif head.left and head.right:
-                sum(head.left, res + head.val)
-                sum(head.right, res + head.val)
+            return sum(head.left, res) or sum(head.right, res)
         
-        sum(root, 0)
-        return True if targetSum in result else False
+        return sum(root, 0)
+    
+    # Runtime : 55 ms, faster than 78.55%
+    # def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+    #     if not root: 
+    #         return False
+    #     result = []
+    #     def sum(head, res):
+    #         if not head.left and not head.right:
+    #             res += head.val
+    #             result.append(res)
+            
+    #         if not head.left and head.right:
+    #             sum(head.right, res + head.val)
+    #         elif not head.right and head.left:
+    #             sum(head.left, res + head.val)
+    #         elif head.left and head.right:
+    #             sum(head.left, res + head.val)
+    #             sum(head.right, res + head.val)
+        
+    #     sum(root, 0)
+    #     return True if targetSum in result else False
