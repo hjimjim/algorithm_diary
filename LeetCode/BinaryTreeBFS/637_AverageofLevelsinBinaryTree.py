838322c60f0ff8c71cc2ac56435a673b8720643b
Jimin Heo <jimjim.heo@gmail.com>
2023-07-11 12:47:48 +0900

    Top Interview 150 (BinaryTreeBFS | Easy | Jul 11, 2023)

---
+++ LeetCode/BinaryTreeBFS/637_AverageofLevelsinBinaryTree.py
@@ -0,0 +1,32 @@
+from typing import Optional
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    """
+        Top Interview 150 (BinaryTree BFS/ Easy)
+        637. Average of Levels in Binary Tree
+        link : https://leetcode.com/problems/average-of-levels-in-binary-tree/
+        Date : July 11, 2023
+        
+        Runtime : 59 ms, faster than 89.4%
+    """
+    def averageOfLevels(self, root: Optional[TreeNode]) -> List[float]:
+        need = [root]
+        res = list()
+        while need:
+            length = len(need)
+            sum_depth = 0
+
+            for i in range(length):
+                node = need.pop(0)
+                sum_depth += node.val
+                if node.left: need.append(node.left)
+                if node.right: need.append(node.right)
+            
+            res.append(sum_depth/length)
+            
+        return res
