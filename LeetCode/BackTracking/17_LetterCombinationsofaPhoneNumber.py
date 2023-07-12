0ed0811d5f6251e7d1a6c6fbbe0ee88ebb907769
Jimin Heo <jimjim.heo@gmail.com>
2023-07-12 12:55:10 +0900

    Top Interview 150 (BackTracking | Medium | Jul 12, 2023) - typical backtracking problem

---
+++ LeetCode/BackTracking/17_LetterCombinationsofaPhoneNumber.py
@@ -0,0 +1,35 @@
+class Solution:
+    """
+        Top Interview 150 (BinaryTree BFS/ Medium)
+        17. Letter Combinations of a Phone Number
+        link : https://leetcode.com/problems/letter-combinations-of-a-phone-number/
+        Date : July 11, 2023
+        
+        Runtime : 46 ms, faster than 62.69%
+    """
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits: 
+            return []
+        
+        res = []
+        phone_digits = {
+            "2" : "abc",
+            "3" : "def",
+            "4" : "ghi",
+            "5" : "jkl",
+            "6" : "mno",
+            "7" : "pqrs",
+            "8" : "tuv",
+            "9" : "wxyz"
+        }
+
+        def backtracking(combination, next):
+            if not next:
+                res.append(combination)
+                return 
+            
+            for letter in phone_digits[next[0]]:
+                backtracking(combination+letter, next[1:])
+            
+        backtracking("", digits)
+        return res
