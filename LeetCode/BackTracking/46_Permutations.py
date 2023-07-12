f75f20f6a93f540fb248f9d4031ec5a572d0d5fb
Jimin Heo <jimjim.heo@gmail.com>
2023-07-12 15:02:47 +0900

    Top Interview 150 (BackTracking | Medium | Jul 12, 2023)

---
+++ LeetCode/BackTracking/46_Permutations.py
@@ -0,0 +1,25 @@
+from typing import List
+class Solution:
+    """
+        Top Interview 150 (BinaryTree BFS/ Medium)
+        46. Permutations
+        link : https://leetcode.com/problems/permutations/
+        Date : July 12, 2023
+        
+        Runtime : 42 ms, faster than 98.61%
+    """
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        
+        def permutation(permu):
+            if len(permu) == len(nums):
+                res.append(permu[:])
+                return 
+            for i in nums:
+                if i not in permu:
+                    permu.append(i)
+                    permutation(permu)
+                    permu.pop()
+        
+        permutation([])
+        return res
