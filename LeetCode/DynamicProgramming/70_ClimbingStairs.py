843406b032b832e44bbb49d7397af4df1586c232
Jimin Heo <jiminheo@dhcp-v078-251.mobile.uci.edu>
2024-04-01 10:51:46 -0700

    Top Interview 150 (DP | Easy | April 1, 2024)

--- LeetCode/DynamicProgramming/70_ClimbingStairs.py
+++ LeetCode/DynamicProgramming/70_ClimbingStairs.py
@@ -4,24 +4,17 @@ class Solution:
             Top Interview 150 (DP/ Easy)
             70. Climbing Stairs
             link: https://leetcode.com/problems/climbing-stairs/
-            Date : July 14, 2023
-        
-            Runtime : 45 ms, faster than 62.07%
-
+            Date : Apr 1, 2024
+    
             n = 1, ans = 1
             n = 2, ans = 2
             n = 3, ans = 3
             dp(n+2) = dp(n) + dp(n+1)
         """
 
-        if n <= 2:
-            return n
-
-        prev1 = 1
-        prev2 = 2
-
-        for _ in range(n-2):
-            curr = prev1 + prev2
-            prev1, prev2 = prev2, curr
+        memo = [1,2]
         
-        return prev2
+        for i in range(2, n):
+            memo.append(memo[i-1] + memo[i-2])
+        
+        return memo[n-1]
